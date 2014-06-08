991e72caad752c81ac72438f319dca0228530851
Thuong Ho <thuong.t.ho@gmail.com>
2014-06-08 02:29:21 -0700

    prehistoric build

--- Gemfile
+++ Gemfile
@@ -68,3 +68,5 @@ group :production do
   gem 'pg'
   gem 'rails_12factor'
 end
+
+gem 'devise'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606082752) do
+ActiveRecord::Schema.define(version: 20140608084356) do
 
   create_table "employer_addresses", force: true do |t|
     t.string   "address_line_one", limit: 50
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20140606082752) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "jobs", force: true do |t|
+    t.integer "minor_id"
+    t.integer "employer_id"
+  end
+
+  add_index "jobs", ["employer_id"], name: "index_jobs_on_employer_id"
+  add_index "jobs", ["minor_id"], name: "index_jobs_on_minor_id"
 
   create_table "minor_addresses", force: true do |t|
     t.string   "address_line_one", limit: 50
@@ -50,7 +58,20 @@ ActiveRecord::Schema.define(version: 20140606082752) do
     t.integer  "grade"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "minors", ["email"], name: "index_minors_on_email", unique: true
+  add_index "minors", ["reset_password_token"], name: "index_minors_on_reset_password_token", unique: true
 
   create_table "school_addresses", force: true do |t|
     t.string   "address_line_one", limit: 50
@@ -68,5 +89,33 @@ ActiveRecord::Schema.define(version: 20140606082752) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "supervisors", force: true do |t|
+    t.string   "first_name",           limit: 25
+    t.string   "last_name",            limit: 25
+    t.string   "position",             limit: 50
+    t.string   "signature_image_path"
+    t.integer  "employer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
